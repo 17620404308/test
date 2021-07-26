cc439b43c7a5e85a85eed4d8d30d0a22741ca44e
username@email.com <977996732@qq.com>
2021-07-27 01:17:13 +0800

    更新falsk

--- Falsk/api/user.py
+++ Falsk/api/user.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify,request
 import re
+import time
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False # jsonify返回的中文正常显示
 from common.mysql_operate import db
+from common.redis_operate import redis_db
 # data = [
 #     {"id": 1, "username": "小明", "password": "123456", "role": 0, "sex": 0, "telephone": "10086", "address": "北京市海淀区"},
 #     {"id": 2, "username": "李华", "password": "abc", "role": 1, "sex": 0, "telephone": "10010", "address": "广州市天河区"},
@@ -34,6 +36,7 @@ def get_user(username):
 
 @app.route("/register", methods=['POST'])
 def user_register():
+    '''注册'''
     username = request.json.get("username").strip() # 用户名
     password = request.json.get("password").strip() # 密码
     sex = request.json.get("sex", "0").strip() # 性别，默认为0(男性)
@@ -61,18 +64,65 @@ def user_register():
         return jsonify({"code": 2001, "msg": "用户名/密码/手机号不能为空，请检查！！！"})
 @app.route("/login", methods=['POST'])
 def user_login():
+    '''登录'''
     username = request.values.get("username")
     password = request.values.get("password")
+    print(username)
     sql1 = "select username from user where username = '{}'".format(username)
     sql_name = db.select_db(sql1)
-    sql2 = "select password from user where username = '{}'".format(username)
-    sql_pwd = db.select_db(sql2)
+    sql2 = "select * from user where username = '{}'".format(username)
+    sql_info = db.select_db(sql2)
     if username and password:
-        if username == sql_name and password == sql_pwd:
-            return jsonify({"code": 0, "msg": "恭喜，登录成功！"})
+        if not sql_name:
+            return jsonify({"code": 1003, "msg": "用户名不存在！"})
+        if sql_info:
+            timeStamp = int(time.time())#获取当前时间戳
+            token = "{}{}".format(username,timeStamp)
+            redis_db.handle_redis_token(username,token)#把token放到Redis中储存
+            login_info = {
+                "id":sql_info[0],
+                "username":username,
+                "token":token,
+                "login_time":time.strftime("%Y/%m/%d %H:%M:%S")
+            }
+            return jsonify({"code": 0,"login_info":login_info, "msg": "恭喜，登录成功！"})
         return jsonify({"code": 1002, "msg": "用户名或密码错误！！！"})
     else:
         return jsonify({"code": 1001, "msg": "用户名或密码不能为空！！！"})
+
+@app.route("/delete/user/<int:id>", methods=['POST'])
+def user_delete(id):
+    username = request.json.get("username", "").strip()  # 当前登录的管理员用户
+    token = request.json.get("token", "").strip()  # token口令
+    if username and token:
+        redis_token = redis_db.handle_redis_token(username)  # 从redis中取token
+        if redis_token:
+            if redis_token == token:  # 如果从redis中取到的token不为空，且等于请求body中的token
+                sql1 = "SELECT role FROM user WHERE username = '{}'".format(username)
+                res1 = db.select_db(sql1)
+                print("根据用户名 【 {} 】 查询到用户类型 == >> {}".format(username, res1))
+                user_role = res1[0]["role"]
+                if user_role == 0:  # 如果当前登录用户是管理员用户
+                    sql2 = "SELECT * FROM user WHERE id = '{}'".format(id)
+                    res2 = db.select_db(sql2)
+                    print("根据用户ID 【 {} 】 查询到用户信息 ==>> {}".format(id, res2))
+                    if not res2:  # 如果要删除的用户不存在于数据库中，res2为空
+                        return jsonify({"code": 3005, "msg": "删除的用户ID不存在，无法进行删除，请检查！！！"})
+                    elif res2[0]["role"] == 0: # 如果要删除的用户是管理员用户，则不允许删除
+                        return jsonify({"code": 3006, "msg": "用户ID：【 {} 】，该用户不允许删除！！！".format(id)})
+                    else:
+                        sql3 = "DELETE FROM user WHERE id = {}".format(id)
+                        db.execute_db(sql3)
+                        print("删除用户信息SQL ==>> {}".format(sql3))
+                        return jsonify({"code": 0, "msg": "恭喜，删除用户信息成功！"})
+                else:
+                    return jsonify({"code": 3004, "msg": "当前用户不是管理员用户，无法进行操作，请检查！！！"})
+            else:
+                return jsonify({"code": 3003, "msg": "token口令不正确，请检查！！！"})
+        else:
+            return jsonify({"code": 3002, "msg": "当前用户未登录，请检查！！！"})
+    else:
+        return jsonify({"code": 3001, "msg": "管理员用户/token口令不能为空，请检查！！！"})
 
 
 
--- Falsk/config/setting.py
+++ Falsk/config/setting.py
@@ -2,8 +2,14 @@
 SERVER_PORT = 9999
 
 # MySQL配置
-MYSQL_HOST = "192.168.94.141"
+MYSQL_HOST = "127.0.0.1"
 MYSQL_PORT = 3306
 MYSQL_USER = "root"
 MYSQL_PASSWD = "123456"
 MYSQL_DB = "flask_demo"
+# Redis配置
+REDIS_HOST = "127.0.0.1"
+REDIS_PORT = 6379
+REDIS_PASSWD = "foobared"
+# token过期时间（单位：秒）
+EXPIRE_TIME = 600
